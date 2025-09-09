f0c6a47ab2c8277194d421e315baef4257cd7463
berriestrawberrie <brooke.henriksson@outlook.com>
2025-09-09 12:46:47 +0200

    Added types to TS file, added placeholders for WeatherAPI in HTML, added loading text callback function to QuoteAPI button

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var _a;
+var quoteReturn = document.getElementById("quote-return");
 //Quote API Start
 var fetchQuoteBy = function () {
     fetch('https://api.adviceslip.com/advice')
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        document.getElementById("quote-return").innerText = data.slip.advice;
+        quoteReturn.innerText = data.slip.advice;
         console.log(data);
     })
         .catch(function (error) {
         console.error('Error fetching advice: , error');
     });
 };
-(_a = document.getElementById("quote")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", fetchQuoteBy);
+(_a = document.getElementById("quote")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    //DISPLAY WAIT MESSAGE
+    quoteReturn.innerText = "loading...";
+    //WAIT 2SEC THEN DO API CALL
+    setTimeout(function () {
+        fetchQuoteBy();
+    }, 2000);
+});
+//fetchQuoteBy();
--- index.ts
+++ index.ts
@@ -1,14 +1,30 @@
+const quoteReturn = document.getElementById("quote-return")!;
+type AdviceResponse = {
+    slip:{
+        id:number;
+        advice:string;
+    }
+}
 //Quote API Start
-const fetchQuoteBy = () => {
+const fetchQuoteBy = ():void => {
     fetch('https://api.adviceslip.com/advice')
-    .then((response) => response.json())
-    .then((data)=>{
-        document.getElementById("quote-return")!.innerText = data.slip.advice;
+    .then((response: Response) => response.json() as Promise<AdviceResponse>)
+    .then((data: AdviceResponse)=>{
+       quoteReturn.innerText = data.slip.advice;
         console.log(data);
     })
-    .catch((error)=>{
+    .catch((error:unknown)=>{
         console.error('Error fetching advice: , error');
     });
 }
 
-document.getElementById("quote")?.addEventListener("click",fetchQuoteBy);
+
+document.getElementById("quote")?.addEventListener("click",()=>{
+    //DISPLAY WAIT MESSAGE
+    quoteReturn.innerText = "loading..."
+    //WAIT 2SEC THEN DO API CALL
+    setTimeout(()=>{
+       fetchQuoteBy(); 
+    },2000);
+});
+//fetchQuoteBy();
