2423d64fc3047407616d2a53fccef71f3f5f2032
berriestrawberrie <brooke.henriksson@outlook.com>
2025-09-10 13:45:57 +0200

    defined task interface and declared empty array list

---
+++ index.ts
@@ -0,0 +1,14 @@
+
+//DEFINE THE TASK INTERFACE
+interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    completed: boolean;
+    priority: "low" | "medium" | "high";
+    createdAt: Date;
+    category: string;
+}
+
+//DECLARE AN EMPTY ARRAY OF TASK OBJECTS
+const tasks: Task[] = [];
